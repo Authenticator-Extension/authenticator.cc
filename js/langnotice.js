4d4ec62a1b58edc4ebbc84c41a3ab578e18fb66d
Brendan Early <mymindstorm1@gmail.com>
2019-04-08 17:39:48 -0500

    add change language notice
    if browser lang is diffrent from page lang && strings are found

---
+++ js/langnotice.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let userLanguage = navigator.languages
+    ? navigator.languages[0]
+    : navigator.language || navigator.userLanguage;
+  const currentLanguage = document.documentElement.lang;
+  const localeURI = "/docs/strings";
+
+  switch (userLanguage) {
+    case "zh-cn":
+      userLanguage = "zh_CN";
+      break;
+    case "zh-tw":
+      userLanguage = "zh_TW";
+      break;
+    case "zh":
+      userLanguage = "zh_TW";
+      break;
+    case "pt-br":
+      userLanguage = "pt_BR";
+      break;
+    default:
+      userLanguage = userLanguage.substring(0, currentLanguage.length);
+      break;
+  }
+
+  if (userLanguage && currentLanguage && userLanguage !== currentLanguage) {
+    const langChangeDiv = document.getElementById("lang-change-div");
+    const langChangeLink = document.getElementById("lang-change-a");
+    const langChangeText = document.getElementById("lang-change-text");
+    const strings = JSON.parse(get(localeURI));
+
+    if (strings[userLanguage].viewInLang && strings[userLanguage].clickHere) {
+      langChangeLink.href = window.location.pathname.replace(
+        `/${currentLanguage}/`,
+        `/${userLanguage}/`
+      );
+      langChangeLink.innerText = strings[userLanguage].clickHere;
+      langChangeText.innerText = strings[userLanguage].viewInLang;
+      langChangeDiv.style = null;
+    } else {
+      console.log("Strings not found for " + userLanguage);
+      console.log("Page is " + currentLanguage);
+    }
+  } else {
+    console.error(`ul: ${userLanguage} cl: ${currentLanguage}`);
+  }
+});
+
+function get(url) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url, false);
+  xhr.send();
+  return xhr.responseText;
+}
